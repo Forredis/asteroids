edea8d023d768f85b25c5e8d309f929a14a04f28
Forredis_Guidal <Timothy.d.henry.19@gmail.com>
2025-12-16 20:56:07 -0500

    C: Created the player ship and made so it could turn left and right

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import pygame 
+from player import Player
 from constants import *
 from logger import log_state
 
 
 def main():
+    # Must run this first in console 'export DISPLAY=localhost:0'
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
 
     while True:
         log_state()
@@ -18,8 +24,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
-    screen.fill("black")
-    pygame.display.flip()
+        dt = clock.tick(60) / 1000
+    
+        screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip()
 
 
 if __name__ == "__main__":

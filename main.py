35f54ffdf6e090f5b36949a454638c7519c8c9d5
Forredis_Guidal <Timothy.d.henry.19@gmail.com>
2025-12-17 20:53:12 -0500

    D: Allowed forward/backward movement of the player and added updates/drawing to groups

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ from logger import log_state
 
 
 def main():
-    # Must run this first in console 'export DISPLAY=localhost:0'
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
 
     while True:
         log_state()
@@ -27,8 +30,11 @@ def main():
         dt = clock.tick(60) / 1000
     
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+
+        for drawing in drawable:
+            drawing.draw(screen)
+
         pygame.display.flip()
 
 

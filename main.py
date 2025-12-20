03826580d149b4c3e6ba8d6893782a49cca7f886
Forredis_Guidal <Timothy.d.henry.19@gmail.com>
2025-12-19 19:14:24 -0500

    G: Player can now shoot bullets that destroy asteroids

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pygame 
 import sys
+
 from player import Player
 from asteroid import Asteroid
+from shot import Shot
 from asteroidfield import AsteroidField
+
 from constants import *
+
 from logger import log_state, log_event
 
 
@@ -16,10 +20,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
     Player.containers = (updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
 
     asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
@@ -45,6 +51,12 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
 
         for drawing in drawable:
             drawing.draw(screen)
